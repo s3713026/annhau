584be3355af2e2b3759af8bad156dab149a8119d
AkadigitalProject <phucuong200297@gmail.com>
2023-06-21 11:32:44 +0700

    1

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'Production'
-? 'http://myapidomain.com/'
+? 'https://demo.akadigital.net/'
 : 'http://localhost:8080/' ;
 
 export default PAYMENT_SERVER_URL;
